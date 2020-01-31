e687236e4a74725d7a64b163806dedbe2f81ceee
kriviears <kriviears@gmail.com>
2020-01-31 18:42:07 -0500

    Updates

---
+++ precache-manifest.e615af72a1b4644eeb8ceb7ad80c1ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90dedaa405b5a2cc1ca2abdb5a53c975",
+    "url": "/react-launch-test/index.html"
+  },
+  {
+    "revision": "9025023814f0355ea4da",
+    "url": "/react-launch-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/react-launch-test/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-launch-test/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9025023814f0355ea4da",
+    "url": "/react-launch-test/static/js/main.c9a17bdf.chunk.js"
+  },
+  {
+    "revision": "42c9a9786cce5da25208",
+    "url": "/react-launch-test/static/js/runtime-main.b9e2a09c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-launch-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
